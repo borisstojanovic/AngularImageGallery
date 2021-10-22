cb02d1fd16c97337a023c92d29bf24f9f4e930ad
boris.stojanovic <boris.stojanovic@msg-global.com>
2021-10-22 15:37:14 +0200

    Added the images related components to the project - initial setup - no real logic or ui

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ImagesComponent } from './images/images.component';
+import { ImagesListComponent } from './images/images-list/images-list.component';
+import { ImageEditComponent } from './images/image-edit/image-edit.component';
+import { ImageDetailsComponent } from './images/image-details/image-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ImagesComponent,
+    ImagesListComponent,
+    ImageEditComponent,
+    ImageDetailsComponent
   ],
   imports: [
     BrowserModule
